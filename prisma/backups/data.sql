f85bf9bdec9f97ecc0334a3a7275f6b394994d0f
karlopareja11-lab <karlopareja11-lab@users.noreply.github.com>
2026-08-09 01:18:42 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict QFJZDKdXBlyOCGnsuxD4cUcxQzbKmi7RhAIlTjk4jHEUe1djvEBrPUBBPLDQMnQ
+-- \restrict 8XVgkrKVcpfKcpHUbzH13PuKhvB6lHeVemHCcce02vYPPgWzbVgQTaQ4tSK3kP0
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -2337,6 +2337,6 @@ SELECT pg_catalog.setval('"pub"."workflow_statistics_id_seq"', 4, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict QFJZDKdXBlyOCGnsuxD4cUcxQzbKmi7RhAIlTjk4jHEUe1djvEBrPUBBPLDQMnQ
+-- \unrestrict 8XVgkrKVcpfKcpHUbzH13PuKhvB6lHeVemHCcce02vYPPgWzbVgQTaQ4tSK3kP0
 
 RESET ALL;
